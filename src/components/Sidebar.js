c27c0d826d58d539fbaad23f55e960f01ad2e22c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-25 19:28:01 +0100

    added files

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,30 +3,33 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <aside className="sidebar">
-      <ul className="sidebar__nav">
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/connections">My network</Link>
-        </li>
-        <li>
-          <Link to="/jobs">Jobs</Link>
-        </li>
-        <li>
-          <Link to="/messages">Messaging</Link>
-        </li>
-        <li>
-          <Link to="/notifications">Notifications</Link>
-        </li>
-        <li>
-          <Link to="/me">Me</Link>
-        </li>
-      </ul>
-    </aside>
+    <div className="sidebar">
+      <nav className="sidebar__nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/network">My Network</Link>
+          </li>
+          <li>
+            <Link to="/jobs">Jobs</Link>
+          </li>
+          <li>
+            <Link to="/messaging">Messaging</Link>
+          </li>
+          <li>
+            <Link to="/notifications">Notifications</Link>
+          </li>
+          <li>
+            <Link to="/me">Me</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
 export default Sidebar;
+
 
