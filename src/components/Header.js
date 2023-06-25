909530d1cedb5dfdd4ad91877ea689c1d03049d9
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-25 19:42:51 +0100

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import SearchBar from '../components/SearchBar';
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">
-        <img src="linkedin_logo.png" alt="LinkedIn Logo" />
+      <div className="header__left">
+        <img className="header__logo" src="linkedin_logo.png" alt="LinkedIn Logo" />
+        <SearchBar />
+        
       </div>
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/network">My Network</Link>
-        <Link to="/jobs">Jobs</Link>
-        <Link to="/messaging">Messaging</Link>
-        {/* Add more navigation links as needed */}
-      </nav>
+      <div className="header__right">
+        <ul className="header__sections">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/network">My Network</a>
+          </li>
+          <li>
+            <a href="/jobs">Jobs</a>
+          </li>
+          <li>
+            <a href="/messaging">Messaging</a>
+          </li>
+          <li>
+            <a href="/notifications">Notifications</a>
+          </li>
+          <li>
+            <a href="/me">Me</a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
